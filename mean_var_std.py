96dcc2e4e7f702c7eb861df68906d2a074a2fb9a
IbrahZo3bi <141644844+IbrahZo3bi@users.noreply.github.com>
2024-11-13 16:58:41 +0300

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,1 +1,30 @@
 
+import numpy as np
+
+def calculate(l):
+    
+
+    if len(l)!=9:
+  
+        raise ValueError('List must contain nine numbers.')
+    
+        return("List must contain nine numbers.")
+    
+    dic={"mean":[],
+         "variance":[],
+         "standard deviation":[],
+         "max":[],
+         "min":[],
+         "sum":[],
+         }
+    
+    l=np.reshape(l,(3,3))
+    dic['mean']=[np.mean(l,axis=0).tolist(),np.mean(l,axis=1).tolist(),np.mean(l).tolist()]
+    dic['variance']=[np.var(l,axis=0).tolist(),np.var(l,axis=1).tolist(),np.var(l).tolist()]
+    dic["standard deviation"]=[np.std(l,axis=0).tolist(),np.std(l,axis=1).tolist(),np.std(l).tolist()]
+    dic['max']=[np.max(l,axis=0).tolist(),np.max(l,axis=1).tolist(),np.max(l).tolist()]
+    dic['min']=[np.min(l,axis=0).tolist(),np.min(l,axis=1).tolist(),np.min(l).tolist()]
+    dic['sum']=[np.sum(l,axis=0).tolist(),np.sum(l,axis=1).tolist(),np.sum(l).tolist()]
+    return dic
+    
+
